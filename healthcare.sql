8c5f746a1bdbf52f5583641fd046069a5d4089a5
moingeek <gadmoin@gmail.com>
2018-03-02 17:01:47 +0530

    Contact added into db

--- healthcare.sql
+++ healthcare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 02, 2018 at 12:13 PM
--- Server version: 5.6.17-log
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Mar 02, 2018 at 05:00 PM
+-- Server version: 5.7.21-0ubuntu0.16.04.1
+-- PHP Version: 7.0.27-1+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `healthcare`
@@ -26,16 +26,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `appointment`
 --
 
-CREATE TABLE IF NOT EXISTS `appointment` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `appointment` (
+  `id` int(12) NOT NULL,
   `username` varchar(255) NOT NULL,
   `patient_name` varchar(255) NOT NULL,
   `appointment_date` date NOT NULL,
   `appointment_of` varchar(255) NOT NULL,
   `appointment_time` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `type` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `appointment`
@@ -50,19 +49,29 @@ INSERT INTO `appointment` (`id`, `username`, `patient_name`, `appointment_date`,
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `gym`
 --
 
-CREATE TABLE IF NOT EXISTS `gym` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `gym` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `appointment_date` date NOT NULL,
   `time_from` timestamp NULL DEFAULT NULL,
   `time_end` timestamp NULL DEFAULT NULL,
-  `trainer` varchar(255) NOT NULL,
-  `package` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `trainer` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -70,25 +79,22 @@ CREATE TABLE IF NOT EXISTS `gym` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(12) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(12) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email_id` varchar(255) NOT NULL,
-  `contact_no` bigint(12) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`,`username`),
-  UNIQUE KEY `email_id` (`email_id`,`contact_no`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `contact_no` bigint(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `email_id`, `contact_no`, `type`) VALUES
-(1, 'testm', 'e10adc3949ba59abbe56e057f20f883e', 'munafhajirpro@gmail.com', 2147483647, ''),
-(2, 'testmu', 'e10adc3949ba59abbe56e057f20f883e', 'test@gmail.com', 2147483647, ''),
-(4, 'testu', 'e10adc3949ba59abbe56e057f20f883e', 'mafon@hiaf', 1234567890, '');
+INSERT INTO `user` (`id`, `username`, `password`, `email_id`, `contact_no`) VALUES
+(1, 'testm', 'e10adc3949ba59abbe56e057f20f883e', 'munafhajirpro@gmail.com', 2147483647),
+(2, 'testmu', 'e10adc3949ba59abbe56e057f20f883e', 'test@gmail.com', 2147483647),
+(4, 'testu', 'e10adc3949ba59abbe56e057f20f883e', 'mafon@hiaf', 1234567890);
 
 -- --------------------------------------------------------
 
@@ -96,7 +102,7 @@ INSERT INTO `user` (`id`, `username`, `password`, `email_id`, `contact_no`, `typ
 -- Table structure for table `validation`
 --
 
-CREATE TABLE IF NOT EXISTS `validation` (
+CREATE TABLE `validation` (
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -107,6 +113,7 @@ CREATE TABLE IF NOT EXISTS `validation` (
 
 INSERT INTO `validation` (`name`, `type`) VALUES
 ('Suraj', 'Hospital'),
+('Suraj', 'Hospital'),
 ('Munaf', 'Gym'),
 ('Moin', 'Yoga');
 
@@ -116,17 +123,75 @@ INSERT INTO `validation` (`name`, `type`) VALUES
 -- Table structure for table `yoga`
 --
 
-CREATE TABLE IF NOT EXISTS `yoga` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `yoga` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `appointment_date` varchar(255) NOT NULL,
-  `time_from` timestamp NOT NULL,
-  `time_end` timestamp NOT NULL,
+  `time_from` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `time_end` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `trainer` varchar(255) NOT NULL,
-  `package` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `package` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`name`);
+
+--
+-- Indexes for table `gym`
+--
+ALTER TABLE `gym`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`,`username`),
+  ADD UNIQUE KEY `email_id` (`email_id`,`contact_no`);
+
+--
+-- Indexes for table `yoga`
+--
+ALTER TABLE `yoga`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `gym`
+--
+ALTER TABLE `gym`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `yoga`
+--
+ALTER TABLE `yoga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
